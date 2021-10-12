fc6719e710811a25ea9a01e5d2151ab196830237
codyuhi <codylkuhi@gmail.com>
2021-10-11 20:29:51 -0600

    Remove build directory from source control

--- v2/backend/dist/controllers/tokens.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=tokens.js.map
--- v2/backend/dist/middleware/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkAdmin = exports.validUser = void 0;
-const ValidUser_1 = require("./ValidUser");
-Object.defineProperty(exports, "validUser", { enumerable: true, get: function () { return ValidUser_1.ValidUser; } });
-const CheckAdmin_1 = require("./CheckAdmin");
-Object.defineProperty(exports, "checkAdmin", { enumerable: true, get: function () { return CheckAdmin_1.CheckAdmin; } });
-//# sourceMappingURL=index.js.map
