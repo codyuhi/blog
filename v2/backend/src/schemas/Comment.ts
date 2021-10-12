002e34eafc0ee01d6a31e593884274912834838a
codyuhi <codylkuhi@gmail.com>
2021-10-11 21:52:37 -0600

    Finish article mongo operations, start user mongo operations

--- v2/backend/src/schemas/Comment.ts
+++ v2/backend/src/schemas/Comment.ts
@@ -1,14 +1,14 @@
-import { Schema } from 'mongoose'
-import { User } from './User'
+// import { Schema } from 'mongoose'
+// import { User } from './User'
 
-export const Comment = new Schema({
-    content: String,
-    articleId: String,
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    user: User
-})
+// export const Comment = new Schema({
+//     content: String,
+//     articleId: String,
+//     created: {
+//         type: Date,
+//         default: Date.now
+//     },
+//     user: User
+// })
 
-Comment
+// Comment
--- v2/backend/src/schemas/User.ts
+++ v2/backend/src/schemas/User.ts
@@ -1,29 +1,18 @@
-import { Schema } from 'mongoose'
-import argon2 from 'argon2'
+import { ObjectId } from 'mongodb'
 
-export const User = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    username: String,
-    password: String,
-    role: {
-        type: String,
-        default: 'user'
-    }
-})
-
-User.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next()
-    }
-    try {
-        const hash = await argon2.hash(this.password)
-        this.password = hash
-        next()
-    } catch (err) {
-        console.error(err)
-    }
-})
+export default class User {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public username: string,
+        public password: string,
+        public role: {
+            type: string,
+            default: 'user',
+            select: false
+        }
+    ) { }
+}
 
 User
